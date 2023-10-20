22d5ecf4caad6d3f6029d88cb8139f6c77ea8197
Oscar Roberts <63606615+roberts6@users.noreply.github.com>
2023-10-20 09:43:50 -0600

    se agrega una 3ra transaccion

--- TCL+Roberts.sql
+++ TCL+Roberts.sql
@@ -51,3 +51,35 @@ ROLLBACK TO ingresos_proveedor2;
 
 commit;
 
+
+-- transacción que elimina y agrega productos por bloques (podría usarse para reemplazar productos por proveedor)
+START TRANSACTION;
+
+SAVEPOINT baja_proveedor1;
+INSERT INTO producto (marca, modelo, talle,fk_idProveedor,fecha_ingreso,stock,precio,genero)
+VALUES('Jordan', 'Air Jordan 1', 8.5, 1,current_timestamp(), 20, 150.00, 'Hombre'),
+('Jordan', 'Air Jordan 4', 9.0, 1,current_timestamp(), 1, 180.00, 'Mujer'),
+('Jordan', 'Air Jordan 11', 10.0,1,current_timestamp(), 12, 170.00, 'Hombre'),
+('Jordan', 'Air Jordan 3', 9.5, 1, current_timestamp(),8, 190.00, 'Mujer');
+
+SAVEPOINT ingresos_proveedor1;
+INSERT INTO producto (marca, modelo, talle, fk_idProveedor, fecha_ingreso, stock, precio, genero)
+VALUES
+('Jordan', 'Air Jordan 6', 10.0, 1, current_timestamp(), 15, 160.00, 'Hombre'),
+('Jordan', 'Air Jordan 12', 9.5, 1, current_timestamp(), 7, 210.00, 'Mujer'),
+('Nike', 'Air Max 90', 8.0, 1, current_timestamp(), 25, 120.00, 'Hombre'),
+('Adidas', 'Superstar', 7.5, 1, current_timestamp(), 30, 90.00, 'Mujer');
+
+
+SELECT pr.nombre AS proveedor, p.fk_idProveedor AS id_proveedor, COUNT(p.fk_idProveedor) AS cantidad
+FROM producto AS p
+JOIN proveedor AS pr ON p.fk_idProveedor = pr.id_proveedor
+GROUP BY p.fk_idProveedor;
+
+
+select * from producto
+where fk_idProveedor = 1;
+
+ROLLBACK TO ingresos_proveedor1;
+
+commit;
